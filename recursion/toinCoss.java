e5ace95b5128141f504c4ce01dd1d194e219a449
Ansh agrawal <agrawalansh624@gmail.com>
2025-02-21 14:16:36 +0530

    recursion

--- recursion/toinCoss.java
+++ recursion/toinCoss.java
@@ -2,6 +2,7 @@ public class toinCoss {
     public static void main(String[] args) {
 
         coinToss(3, "");
+        notHead(3, "");
         
     }
 
@@ -14,4 +15,22 @@ public class toinCoss {
         coinToss(n-1, ans+'H');
         coinToss(n-1, ans+'T');
     }
+
+    public static void notHead(int n , String ans){
+
+        if(n==0){
+            System.out.println(ans);
+            return;
+        }
+        
+        if(ans.length()==0 || ans.charAt(ans.length()-1) != 'H'){
+            notHead(n-1, ans+'H');
+        }
+        notHead(n-1, ans+'T');
+        
+    }
 }
+
+
+// extended question -->
+// print only those sequences which doesn't contain consecutive heads
