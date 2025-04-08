bd2d849047254579a64c006325747a64369f826b
Ansh agrawal <agrawalansh624@gmail.com>
2025-04-08 12:15:05 +0530

    backtraccking

---
+++ Backtracking/Partition_Equal_Subset_Sum.java
@@ -0,0 +1,35 @@
+package Backtracking;
+
+public class Partition_Equal_Subset_Sum {
+
+    public static void main(String[] args) {
+
+        int nums [] = {1,5,11,5};
+
+        int sum = 0;
+        for (int i : nums) sum += i;
+        if (sum % 2 != 0) System.out.println(false);
+
+        int target = sum / 2;
+        Boolean[][] dp = new Boolean[nums.length][target + 1];
+
+        boolean ans =  canPartitionHelper(nums, 0, target, dp);
+
+        System.out.println(ans);
+        
+    }
+
+    public static boolean canPartitionHelper(int[] nums, int index, int sum, Boolean[][] dp) {
+        if (sum == 0) return true;
+        if (index >= nums.length || sum < 0) return false;
+
+        if (dp[index][sum] != null) return dp[index][sum];
+
+        // include current element or exclude
+        boolean include = canPartitionHelper(nums, index + 1, sum - nums[index], dp);
+        boolean exclude = canPartitionHelper(nums, index + 1, sum, dp);
+
+        return dp[index][sum] = include || exclude;
+    }
+    
+}
---
+++ tree/binaryTree/Diameter_of_Binary_tree.java
@@ -0,0 +1,17 @@
+package binaryTree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+    }
+}
