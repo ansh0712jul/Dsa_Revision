1e907ca1c5d93898c5a6804a7af3b3e10a69c12b
Ansh agrawal <agrawalansh624@gmail.com>
2025-05-26 14:19:42 +0530

    backtracking

---
+++ Backtracking/String_partitioning.java
@@ -0,0 +1,24 @@
+public class String_partitioning {
+    public static void main(String[] args) {
+
+        String ques = "nitin";
+        print(ques , "");
+        
+    }
+
+    public static void print(String ques , String ans){
+
+        if(ques.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for( int i=1; i<=ques.length(); i++){
+
+            String sub = ques.substring(0 , i);
+
+            print(ques.substring(i) , ans + sub + "|");
+        }
+
+    }
+}
