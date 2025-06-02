2b1b8f4205bd709b2d8195068da5ca2cb2b0ebb0
Ansh agrawal <agrawalansh624@gmail.com>
2025-06-03 00:29:00 +0530

    2D array

---
+++ 2D_Array/RotateImaga.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+//  first transpose the matrix then swap the matrix column wise from 0 to n-1 and n-1 to 0
+
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i ; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+
+            // two swap a aaray we need only n/2 iteration 
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
