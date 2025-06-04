a02e392373a1a94abba9a868eac7f7d956fbf757
Ansh agrawal <agrawalansh624@gmail.com>
2025-06-05 02:07:29 +0530

    update

---
+++ stack&queue/RemoveAllAdjacentDuplicatesIn_String.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            if(!st.isEmpty() && st.peek() == s.charAt(i)) {
+                st.pop();
+                continue;
+            }
+            else st.push(s.charAt(i));
+        }
+
+         StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
+
